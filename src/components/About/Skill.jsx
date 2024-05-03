33acd70eedf782e4c4b4d0e653733327459b7f37
Abdurashitov beksultan <beksultan1709@gmail.com>
2024-05-03 13:14:51 +0300

    added main

--- src/components/About/Skill.jsx
+++ src/components/About/Skill.jsx
@@ -1,11 +1,81 @@
-import React from 'react'
+// import React from 'react'
 
-const Skill = () => {
+// const Skill = () => {
+//   return (
+//     <div>
+//       skill
+//     </div>
+//   )
+// }
+
+// export default Skill
+
+
+
+import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+import {
+  DiJavascript1,
+  DiReact,
+  DiNodejs,
+  DiCss3,
+  DiGit,
+  DiGithub,
+  DiHtml5,
+} from 'react-icons/di';
+import { FaSass } from 'react-icons/fa';
+import {
+  SiFirebase,
+  SiFigma,
+  SiRedux,
+  SiTypescript,
+  SiAdobephotoshop,
+} from 'react-icons/si';
+
+function Skill() {
   return (
-    <div>
-      skill
-    </div>
-  )
+    <Row style={{ justifyContent: 'center', paddingBottom: '50px' }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiHtml5 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiCss3 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiJavascript1 />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiReact />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiRedux />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiFirebase />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGit />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGithub />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiFigma />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiNodejs />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiTypescript />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <FaSass />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiAdobephotoshop />
+      </Col>
+    </Row>
+  );
 }
 
-export default Skill
+export default Skill;
