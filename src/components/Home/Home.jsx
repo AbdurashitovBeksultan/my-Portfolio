e833c5d696da0214fa462b252b1aa3a5fee50b3a
Abdurashitov beksultan <beksultan1709@gmail.com>
2024-05-05 15:30:55 +0300

    added main

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -14,12 +14,16 @@ function Home() {
           <Row>
             <Col md={7} className="home-header">
               <h1 style={{ paddingBottom: 15 }} className="heading">
-                Hi There!{" "}
+               {" "}
                 <span className="wave" role="img" aria-labelledby="wave">
                   👋🏻
                 </span>
               </h1>
 
+              <h1 className="heading-name">
+              Абдурашитов
+                <strong className="main-name">   Бексултан Frontend Разработчик</strong>
+              </h1>
               <h1 className="heading-name">
                 I'M
                 <strong className="main-name"> Abdurashitov Beksultan</strong>
